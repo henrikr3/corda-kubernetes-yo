f901e1c362953f560c45943e14f2f57b08cfb079
Henrik Carlström <43668328+henrikr3@users.noreply.github.com>
2019-03-13 10:57:29 +0000

    Fix typo in copy command

--- fetch_corda_jar.sh
+++ fetch_corda_jar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -o party-a/corda.jar https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda/4.0/corda-4.0.jar
-copy party-a/corda.jar party-b/corda.jar 
-copy party-a/corda.jar party-c/corda.jar 
+cp party-a/corda.jar party-b/corda.jar 
+cp party-a/corda.jar party-c/corda.jar 
